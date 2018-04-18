b6fb2496233cb35cdbe3a405d0a55abe0e84715d
Guillermo Garcia Pascual <36395081+memopein@users.noreply.github.com>
2018-04-17 21:09:19 -0500

    Add files via upload
    ahi esta prros :v si se pudo

---
+++ conexion2.java
@@ -0,0 +1,80 @@
+
+package libray;
+ 
+import java.sql.*;
+ 
+public class conexion2 {
+
+ Connection conect=null;
+
+    public Connection connectDatabase() {
+        try {
+
+            try { 
+                Class.forName("org.postgresql.Driver");
+            } catch (ClassNotFoundException ex) {
+                System.out.println("Error al registrar el driver de PostgreSQL: " + ex);
+            }
+            Connection connection = null;
+            // Database connect
+            // Conectamos con la base de datos
+            connection = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5435/sistemaTusug",
+                    "postgres", "Kurama14");
+            conect=connection;
+ 
+            boolean valid = connection.isValid(50000);
+            System.out.println(valid ? "TEST OK" : "TEST FAIL");
+        } catch (java.sql.SQLException sqle) {
+            System.out.println("Error: " + sqle);
+           
+        }
+         return conect;
+    }
+ 
+    /**
+     * Method to connect to the database by passing parameters.
+     * Método para establecer la conexión a la base de datos mediante el paso de parámetros.
+     * 
+     * @param host <code>String</code> host name or ip. Nombre del host o ip.
+     * @param port <code>String</code> listening database port. Puerto en el que escucha la base de datos.
+     * @param database <code>String</code> database name for the connection. Nombre de la base de datos para la conexión.
+     * @param user <code>String</code> user name. Nombre de usuario.
+     * @param password  <code>String</code> user password. Password del usuario.
+     */
+    public void connectDatabase(String host, String port, String database,
+            String user, String password) {
+        String url = "";
+        try {
+            // We register the PostgreSQL driver
+            // Registramos el driver de PostgresSQL
+            try {
+                Class.forName("org.postgresql.Driver");
+            } catch (ClassNotFoundException ex) {
+                System.out.println("Error al registrar el driver de PostgreSQL: " + ex);
+            }
+            Connection connection = null;
+            url = "jdbc:postgresql://" + host + ":" + port + "/" + database;
+            // Database connect
+            // Conectamos con la base de datos
+            connection = DriverManager.getConnection(
+                    url,
+                    user, password);           
+            boolean valid = connection.isValid(50000);
+            System.out.println(valid ? "TEST OK" : "TEST FAIL");
+        } catch (java.sql.SQLException sqle) { 
+            System.out.println("Error al conectar con la base de datos de PostgreSQL (" + url + "): " + sqle);
+        }
+    }
+ 
+    /**
+     * Testing Java PostgreSQL connection with host and port
+     * Probando la conexión en Java a PostgreSQL especificando el host y el puerto.
+     * @param args the command line arguments
+     */
+    /*public static void main(String[] args) {
+        conexion2 javaPostgreSQLBasic = new conexion2();
+        javaPostgreSQLBasic.connectDatabase();
+        //javaPostgreSQLBasic.connectDatabase("localhost", "5435", "sistemaTusug","postgres", "Administrador");
+    }*/
+}
---
+++ rol.java
@@ -0,0 +1,110 @@
+package access;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import libray.conexion2;
+public class rol {
+     conexion2 c2= new conexion2();
+    Connection cn=c2.connectDatabase();
+    
+    public rol(){
+
+    }
+    
+     public void cargar(){
+     try{
+          String [] registros= new String[3];
+
+        String cons="select * from sistemaTusug.rol" ;
+        
+            Statement st= cn.createStatement();
+            ResultSet rs = st.executeQuery(cons);
+            while(rs.next()){
+                registros[0]=rs.getString(1);
+                System.out.print(registros[0]);
+                registros[1]=rs.getString(2);
+                System.out.print(registros[1]);
+                registros[2]=rs.getString(3);
+                System.out.print(registros[2]);
+                registros[3]=rs.getString(4);
+                System.out.print(registros[3]);
+                
+                
+            }
+            
+     }catch(Exception e){
+                System.out.println(e.getMessage());
+                 }
+ }
+
+
+   
+    
+    public void insert(int id_rol,String descripcion, String nivel){
+     
+    String sql="insert into sistemaTusug.rol (codigo_producto, descripcion_producto, cantidad_producto) values (?,?,?)";
+    try{
+         PreparedStatement pst  = null;
+     
+         pst  = cn.prepareStatement(sql);
+         pst.setInt(1, id_rol);
+         pst.setString(2, descripcion);
+         pst.setString(3, nivel);
+         int n=pst.executeUpdate();
+         if(n>0){
+         JOptionPane.showMessageDialog(null, "Registro Guardado con Exito");
+           
+            }
+        
+    }catch (SQLException ex) {
+            Logger.getLogger(Access.class.getName()).log(Level.SEVERE, null, ex);
+        }
+}
+    
+    public void borrar(int x){
+    try
+{
+ Statement st= cn.createStatement();
+String sql;
+sql="delete * from sistemaTusug.rol";
+st.executeUpdate(sql);
+
+}
+catch(Exception e){}
+
+}
+  //update
+public  void update (String nivel, String descripcion,int id_rol)
+throws SQLException
+{
+  try
+  {
+      
+    // create our java preparedstatement using a sql update query
+    PreparedStatement ps =null;
+    ps=cn.prepareStatement(
+      "UPDATE sistemaTusug.rol SET descripcion = ?, nivel = ? WHERE id_rol = ?");
+
+    // set the preparedstatement parameters
+    ps.setString(1,descripcion);
+    ps.setString(2, nivel);
+    ps.setInt(3,id_rol);
+   
+  
+
+    // call executeUpdate to execute our sql update statement
+    ps.executeUpdate();
+    ps.close();
+  }
+  catch (SQLException se)
+  {
+    // log the exception
+    throw se;
+  }
+}   
+}
