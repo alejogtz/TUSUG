4eec161c049d33fb96abad2270e2b47f105626d9
Alekhius <alejo.gutierrez@itoaxaca.edu.mx>
2018-04-26 00:57:58 -0500

    Movi clases de Login

--- TUSUG/src/GUI/GUIAutobuses.java
+++ TUSUG/src/GUI/GUIAutobuses.java
@@ -1,13 +1,15 @@
 package GUI;
 import GUI.Builder;
+
 import javax.swing.JFrame;
 
 public class GUIAutobuses extends JFrame{
-    //String logo_
+    private static ControladorAutobuses controlador;
+    
     public GUIAutobuses(){
         
-        Builder.construirFrame(this, "Modulo Autobuses", 700, 600, 200, 50, false);
-        Builder.lblImg(this, "C:\\Lab\\Git\\TUSUG\\Recursos\\Ventanas mejoradas\\Imagen de iconos\\tusug logo 116x42.png", 293, 100, 232,99);
+        Builder.construirFrame(this, "Modulo Autobuses", 700, 600, 200, 50, true);
+        Builder.lblImg(this, "src/Imagenes/logo_tusug.png", 293, 100, 232,99);
         this.setVisible(true);
     }
     
