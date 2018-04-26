2a7328bf91f721a7718302fad702016143c7fd0c
Alekhius <alejo.gutierrez@itoaxaca.edu.mx>
2018-04-26 00:22:13 -0500

    Movi carpeta Recursos imagenes

--- TUSUG/src/GUI/GUIAutobuses.java
+++ TUSUG/src/GUI/GUIAutobuses.java
@@ -3,7 +3,9 @@ import GUI.Builder;
 import javax.swing.JFrame;
 
 public class GUIAutobuses extends JFrame{
+    //String logo_
     public GUIAutobuses(){
+        
         Builder.construirFrame(this, "Modulo Autobuses", 700, 600, 200, 50, false);
         Builder.lblImg(this, "C:\\Lab\\Git\\TUSUG\\Recursos\\Ventanas mejoradas\\Imagen de iconos\\tusug logo 116x42.png", 293, 100, 232,99);
         this.setVisible(true);
