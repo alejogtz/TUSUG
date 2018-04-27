d1e659d7d76dc8310722ac21a5927f8ce37d0614
Alekhius <alejo.gutierrez@itoaxaca.edu.mx>
2018-04-26 22:21:55 -0500

    Merge

--- TUSUG/src/GUI/PanelImagen.java
+++ TUSUG/src/GUI/PanelImagen.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package GUI;
 
 import java.awt.Dimension;
@@ -35,14 +30,7 @@ public class PanelImagen extends JPanel {
 
     @Override
     public void paint(Graphics g) {
+        super.paint(g);
         g.drawImage(imagen, 0, 0, this.getWidth(), this.getHeight(), null);
-    }
-
-    public static void main(String[] args) {
-        System.out.println("SSS");
-        JFrame x = new JFrame("Hi");
-        PanelImagen p = new PanelImagen("src/Imagenes/logo_tusug.png");
-        Builder.construirFrame(x,"Hi", 100, 200, 100, 50, true);
-        Builder.add(x, p, new Rectangle(0,0, 100,100));
     }
 }
