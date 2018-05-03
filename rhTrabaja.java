f08cb315d33db146dd6da8b1a40fea6a0fca9b62
Guillermo Garcia Pascual <36395081+memopein@users.noreply.github.com>
2018-05-03 01:15:44 -0500

    Add files via upload

---
+++ rhTrabaja.java
@@ -0,0 +1,14 @@
+package GUI;
+
+import javax.swing.JFrame;
+
+
+public class rhTrabaja extends JFrame {
+    
+   public rhTrabaja(){
+      Builder.construirFrame(this, "Recursos Humanos", 700, 600, 200, 50, false);
+
+   }
+   
+   
+}
