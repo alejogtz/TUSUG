91b74a15ddf6cd453d36661a98133066677f4e4b
Alekhius <alejo.gutierrez@itoaxaca.edu.mx>
2018-05-03 16:41:22 -0500

    Login version Alejo

--- TUSUG/src/GUI/login.java
+++ TUSUG/src/GUI/login.java
@@ -1,3 +1,4 @@
+
 import GUI.Builder;
 import GUI.PanelImagen;
 import java.awt.Color;
@@ -7,24 +8,28 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-
 public class login {
-    
-    public login(){  
-        JFrame x = Builder.construirFrame("NULL", new Rectangle(0,0, 700,600), true);        
-        JPanel p = Builder.crearPanel(x, new Rectangle(0,0, 700,600), "src/Imagenes/login.png", false);
-        
-                
-       JTextField jt= Builder.crearTextField(p, new Rectangle(190,231, 293,38),"", null, null, null,true, false , true);
-       
-       JTextField jt2= Builder.crearTextField(p,new Rectangle(190,231, 293,38), "", null, null, null,true, false, true);
-       
-       JButton b= Builder.crearBoton(p, "Ingresar", new Rectangle(257,380, 185,39),null, false,false);
+
+    JFrame x;
+    JPanel p;
+    JTextField jt;
+    JTextField jt2;
+    JButton b;
+
+    public login() {
+        x = Builder.construirFrame("NULL", new Rectangle(0, 0, 700, 600), true);
+        p = Builder.crearPanel(x, new Rectangle(0, 0, 700, 600), "src/Imagenes/login.png", false);
+
+        jt = Builder.crearTextField(p, new Rectangle(190, 231, 293, 38), "", null, null, null, true, true, true);
+
+        jt2 = Builder.crearTextField(p, new Rectangle(190, 231, 293, 38), "", null, null, null, true, true, true);
+
+        b = Builder.crearBoton(p, "Ingresar", new Rectangle(257, 380, 185, 39), null, false, false);
 
     }
-    
-     public static void main(String[] args) {
-         login l= new login();
+
+    public static void main(String[] args) {
+        login l = new login();
     }
-    
+
 }
