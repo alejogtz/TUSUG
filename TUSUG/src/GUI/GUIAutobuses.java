b24e88d7f796ca18a832b3f94dc8956b24c478b4
Alekhius <alejo.gutierrez@itoaxaca.edu.mx>
2018-05-02 22:17:37 -0500

    Builder Mejorado. Utilizarlo

--- TUSUG/src/GUI/GUIAutobuses.java
+++ TUSUG/src/GUI/GUIAutobuses.java
@@ -5,14 +5,9 @@ public class GUIAutobuses extends JFrame{
     private static ControladorAutobuses controlador;
     
     public GUIAutobuses(){
-        
-//<<<<<<< HEAD
         Builder.construirFrame(this, "Modulo Autobuses", 700, 600, 200, 50, false);
         Builder.lblImg(this, "src/Imagenes/logo_tusug.png", 293, 100, 232,99);
-//=======
-        Builder.construirFrame(this, "Modulo Autobuses", 700, 600, 200, 50, true);
-        Builder.lblImg(this, "src/Imagenes/logo_tusug.png", 293, 100, 232,99);
-//>>>>>>> 4eec161c049d33fb96abad2270e2b47f105626d9
+
         this.setVisible(true);
     }
     
